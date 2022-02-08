5ffb2b9ac12224cad2934507793ec3e094dc16ad
maru_koch <nwokochafranklyn@gmail.com>
2022-02-08 23:33:31 +0100

    Algorithm refactored and tested: running smoothly

---
+++ quadraticSolver.py
@@ -0,0 +1,26 @@
+from math import sqrt as square_root
+
+def quadraticSolver(a, b, c):
+    discriminant = b ** 2 - ( 4 * a * c )
+    try:
+        if discriminant > 0:
+            solution_1 = (-b + square_root(discriminant)) // (2 * a)
+            solution_2 = (-b - square_root(discriminant)) // (2 * a)
+            return solution_1, solution_2
+        elif discriminant == 0:
+            solution = - b // (2 * a)
+            return solution
+        else:
+            return "This quadratic equation has no solution"
+    except ZeroDivisionError as err:
+        return "Error:", err
+   
+        
+    
+    
+
+
+print(quadraticSolver(1, -6, 5))
+
+
+
